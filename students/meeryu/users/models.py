32f65d42dc32b360669f5098704983c8431e0955
Meeryu <meeryu@Meeryuui-MacBook-Pro.local>
2022-03-18 15:00:15 +0900

    Modify: users app column 추가 완료

--- students/meeryu/users/models.py
+++ students/meeryu/users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    name         = models.CharField(max_length = 100)
-    email        = models.CharField(max_length = 100, unique=True)
-    password     = models.CharField(max_length = 1000)
-    phone_number = models.CharField(max_length = 100)
+    name         = models.CharField(max_length=100)
+    email        = models.CharField(max_length=100, unique=True)
+    password     = models.CharField(max_length=1000)
+    phone_number = models.CharField(max_length=100)
     created_at   = models.DateTimeField(auto_now_add=True)
     updated_at   = models.DateTimeField(auto_now=True)
 
