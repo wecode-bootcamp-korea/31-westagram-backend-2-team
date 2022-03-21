4c79084787a82d24723f2c691105431f87880013
Meeryu <meeryu@Meeryuui-MacBook-Pro.local>
2022-03-21 14:41:41 +0900

    Modify: users 앱 앤드포인트, view 클래스명 수정 완료

--- students/meeryu/users/urls.py
+++ students/meeryu/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from users.views import UserView
+from users.views import SignUpView
 
 urlpatterns=[
-    path('/userlist', UserView.as_view())
+    path('/signup', SignUpView.as_view())
 ]
--- students/meeryu/users/views.py
+++ students/meeryu/users/views.py
@@ -6,10 +6,10 @@ from django.views import View
 
 from .models import User
 
-class UserView(View):
+class SignUpView(View):
     def post(self,request):
         data = json.loads(request.body)
-        try: 
+        try:
             REGEX_EMAIL    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
             REGEX_PASSWORD = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
 
@@ -17,13 +17,13 @@ class UserView(View):
             password = data['password']
  
             if not re.match(REGEX_EMAIL, email):
-                return JsonResponse({"message" : "INVALID_EMAIL"}, status=400)
+                return JsonResponse({"message" : "INVALID_EMAIL"}, status = 400)
 
-            if User.objects.filter(email=email):
-                return JsonResponse({"message" : "THIS EMAIL ALREADY EXISTS"}, status=400)
+            if User.objects.filter(email = email):
+                return JsonResponse({"message" : "EMAIL_ALREADY_EXISTS"}, status = 400)
 
-            if not re.match(REGEX_PASSWORD,password):
-                return JsonResponse({"message" : "PLEASE CHECK PASSWORD AGAIN"}, status=400)
+            if not re.match(REGEX_PASSWORD, password):
+                return JsonResponse({"message" : "INVALID_PASSWORD"}, status = 400)
             
             User.objects.create(
                 name         = data['name'],
@@ -31,7 +31,7 @@ class UserView(View):
                 password     = data['password'],
                 phone_number = data['phone_number']
             )
-            return JsonResponse({"message":"SUCCESS"}, status=201)
+            return JsonResponse({"message" : "SUCCESS"}, status = 201)
         except KeyError:
-            return JsonResponse({"message": "KEY_ERROR"}, status=400) 
+            return JsonResponse({"message" : "KEY_ERROR"}, status = 400) 
             
