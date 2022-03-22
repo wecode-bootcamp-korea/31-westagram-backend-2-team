1c22b598a989bdf8ca5e0c0fc2ce53d547c2e2eb
Meeryu <meeryu@Meeryuui-MacBook-Pro.local>
2022-03-22 17:11:54 +0900

    Modify: users 앱/views.py 오타 추가 수정

--- students/meeryu/users/views.py
+++ students/meeryu/users/views.py
@@ -51,7 +51,7 @@ class SignInView(View):
                 if not User.objects.filter(email = email, password = password):
                     return JsonResponse({"message" : "INVALID_USER"}, status = 401)
 
-                return JsonResponse({"message" : "SUCCESS"}, status=201)
+                return JsonResponse({"message" : "SUCCESS"}, status = 201)
 
             except KeyError:
                 return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
