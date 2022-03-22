1e6a2985d6e7cdeeb2b925f722d12ed4fbc815fc
ks <kspark607@gmail.com>
2022-03-22 16:30:11 +0900

    Modify: 코드 수정

--- students/kwangsoo/users/views.py
+++ students/kwangsoo/users/views.py
@@ -5,6 +5,7 @@ from django.views import View
 from django.core.exceptions import ObjectDoesNotExist
 
 from users.models import User
+from my_settings import SECRET_KEY, ALGORITHM
 
 class RegistrationView(View):
     def post(self, request):
@@ -14,19 +15,25 @@ class RegistrationView(View):
             name         = data['name']
             email        = data['email']
             password     = data['password']
-            hashed_pw    = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-            hashed_pw    = hashed_pw.decode('utf-8')
             phone_number = data['phone_number']
            
             REGEX_EMAIL    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
             REGEX_PASSWORD = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
 
         
+
             if not re.match(REGEX_EMAIL, email):
                 return JsonResponse({'Message' : 'INVAILD_EMAIL'}, status = 400)
 
             if not re.match(REGEX_PASSWORD, password):
                 return JsonResponse({'Message' : 'INVALID_PASSWORD'}, status = 400)
+
+            if User.objects.filter(email = email).exists():
+                return JsonResponse({'Message' : 'REGISTERED_EMAIL'}, status = 400)
+
+            hashed_pw    = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            hashed_pw    = hashed_pw.decode('utf-8')
+
 
             User.objects.create(
                 name         = name,
@@ -46,23 +53,20 @@ class LoginView(View):
         try:
             data = json.loads(request.body)
 
-            email     = data['email']
-            password  = data['password']
-
-            user = User.objects.get(email=eamil)
-
+            email       = data['email']
+            password    = data['password']
+            user        = User.objects.get(email=email)
             db_password = user.password
-
-            if not User.objects.get(email=email):
-                return JsonResponse({"Message" : "EMAIL_NOT_REGISTERED"}, status = 401)
 
             if not bcrypt.checkpw(password.encode('utf-8'), db_password.encode('utf-8')):
                 return JsonResponse({"Message" : "INVLID_PASSWORD"}, status = 401)
 
-            SECRET = 
-            access_token = jwt.encode({"id" : user.id, SECRET, algorithm = "HS256"})
+            access_token = jwt.encode({"id" : user.id}, SECRET_KEY, algorithm = ALGORITHM)
 
-            return JsonResponse({"Message" : "SUCCESS"}, {"token" = access_token} , status = 200)
+            return JsonResponse({"Token" : access_token} , status = 200)
 
         except KeyError:
             return JsonResponse({"Message" : "KEY_ERROR"}, status = 400)
+
+        except User.DoesNotExist:
+            return JsonResponse({"Message" : "EMAIL_NOT_REGISTERED"}, status = 401)
