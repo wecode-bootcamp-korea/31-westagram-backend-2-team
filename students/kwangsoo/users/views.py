d5ae3127c07977064bc4515ead82f79bd84a2dd8
ks <kspark607@gmail.com>
2022-03-22 12:45:16 +0900

    Modify: 미가입 이메일 에러처리 메소드 변경

--- students/kwangsoo/users/views.py
+++ students/kwangsoo/users/views.py
@@ -48,13 +48,10 @@ class LoginView(View):
             email    = data['email']
             password = data['password']
 
-            if (User.objects.get(email = email)).password != password:
+            if not User.objects.filter(email = email, password = password):
                 return JsonResponse({"message" : "INVALID_USER"}, status = 401)
 
             return JsonResponse({"message" : "SUCCESS"}, status = 200)
 
         except KeyError:
             return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
-
-        except User.DoesNotExist:
-            return JsonResponse({"message" : "INVALID_USER"}, status = 401)
