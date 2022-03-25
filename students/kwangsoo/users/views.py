0d72aa902af40e4a40c80b746bb4fe273795064c
ks <kspark607@gmail.com>
2022-03-25 10:06:50 +0900

    Modify: views.py 수정

--- students/kwangsoo/users/views.py
+++ students/kwangsoo/users/views.py
@@ -64,7 +64,7 @@ class LoginView(View):
             return JsonResponse({"message" : "SUCCESS", "token" : access_token}, status = 200)
 
         except KeyError:
-            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
+            return JsonResponse({"message" : "KEY_ERROR"},  status = 400)
 
         except User.DoesNotExist:
             return JsonResponse({"message" : "INVALID_USER"}, status = 401)
